57d33a30ccd25b031de6463011cb16a504012a15
Kisuke <nonokisuke0817@gmail.com>
2020-05-29 21:54:19 +0900

    add animation on dragover, dragenter, drop

--- script.js
+++ script.js
@@ -138,25 +138,22 @@ for (let i = 0; i < list_items.length; i++){
     }
 }
 
-for(let j = 0; j < lists.length; j++){
-    const list = lists[j];
+for(let j = 0; j < banmen_lists.length; j++){
+    const banmen_list = banmen_lists[j];
 
-    list.addEventListener("dragover", function(e){
+    banmen_list.addEventListener("dragover", function(e){
         e.preventDefault();
 
     });
-    list.addEventListener("dragenter", function(e){
+    banmen_list.addEventListener("dragenter", function(e){
         e.preventDefault();
-    });
-}
-
-for(let k = 0; k < fifty_lists.length; k++){
-    const fifty_list = fifty_lists[k];
-
-    fifty_list.addEventListener("dragleave", function(e){
-        e.preventDefault();
+        this.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
     });
 
+    banmen_list.addEventListener("dragleave", function(e){
+        e.preventDefault();
+        this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+    });
 }
 
 for(let l = 0; l < banmen_lists.length; l++) {
@@ -164,6 +161,7 @@ for(let l = 0; l < banmen_lists.length; l++) {
 
     banmen_list.addEventListener("drop", function(e){
         e.preventDefault();
+        this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
         copy = draggedItem.cloneNode(true);
         copy.classList.add("input");
         if(this.firstElementChild != null){
