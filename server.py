952113f1e2ab715bf69cf911f76dc7d3b1ba08a1
n2-freevas <nonodethcloud@icloud.com>
2020-05-31 01:01:54 +0900

    n2 introduced server protocol that Running Banmen request to Satisfied response for returning list

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+import WordCandidate
+
 app = Flask(__name__,static_folder='static')
 
 @app.route('/')
@@ -8,8 +10,9 @@ def hello():
 @app.route('/register', methods=['POST'])
 def post():
     banmen_string = request.form['banmen']
-    return render_template('result.html',posts=banmen_string)
+    return_word = WordCandidate.candidator(target=banmen_string)
 
+    return render_template('result.html',posts = return_word)
     
 if __name__ == "__main__":
    
