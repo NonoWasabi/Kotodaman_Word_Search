f796ca63048e9a20d626ac14b4822caa138f5203
n2-freevas <nonodethcloud@icloud.com>
2020-05-31 21:34:00 +0900

    n2 creating HowtoUse and Funding page

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template,request
 import WordCandidate
-
+import payjp
+import os
+"""
+SECRET_KEY = os.environ['SECRET_KEY']
+PUBLIC_KEY = os.environ['PUBLIC_KEY']
+payjp.api_key = SECRET_KEY
+"""
 app = Flask(__name__,static_folder='static')
 
 @app.route('/')
@@ -13,7 +19,23 @@ def post():
     return_word = WordCandidate.candidator(target=banmen_string)
 
     return render_template('result.html',posts = return_word)
-    
+"""
+@app.route('/funding',method=['POST'])
+def pay():
+    #カード情報を入力し，決済確定ボタンを押してから行われるフェーズ
+    amount = request.form['amount']
+    customer = payjp.Customer.create(
+       card=request.form['payjp-token']
+    )
+    payjp.Charge.create(
+        amount=amount,
+        currency='jpy',
+        customer=customer.id,
+        description='Funding from Bakuzetsu site user'
+    )
+    return render_template('pay.html', amount=amount)
+"""
+
 if __name__ == "__main__":
    
     app.run(debug=True,port=5555)
---
+++ static/showpage.js
@@ -0,0 +1,9 @@
+function HowToUseShow(){
+        
+
+
+}
+
+function FundingPageShow(){
+
+}
